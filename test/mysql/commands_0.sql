b1a66028f84e7adef3fada198ce9da50d9adf31d
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-15 18:30:25 -0500

    Update commands_0.sql

--- test/mysql/commands_0.sql
+++ test/mysql/commands_0.sql
@@ -23,15 +23,15 @@ VALUES ('movie','Beauty and the Beast',1991,84,'disney',1000);
 INSERT INTO Event (eventType,name,year,duration,studioShortName,eventLicensingFee) 
 VALUES ('ppv','MMA Championship',2020,121,'espn',8800);
 
-create_stream,apv,Amazon Prime Video,12
-create_stream,net,Netflix,14
+--create_stream,apv,Amazon Prime Video,12
+--create_stream,net,Netflix,14
 
-offer_movie,apv,Mulan,1998
-offer_movie,apv,The Little Mermaid,1989
-offer_movie,apv,Beauty and the Beast,1991
-offer_ppv,net,30 for 30: Monaco,2020,57
+--offer_movie,apv,Mulan,1998
+--offer_movie,apv,The Little Mermaid,1989
+--offer_movie,apv,Beauty and the Beast,1991
+--offer_ppv,net,30 for 30: Monaco,2020,57
 
-watch_event,age_40_50,30,apv,Mulan,1998
-watch_event,age_40_50,50,apv,Beauty and the Beast,1991
-watch_event,age_40_50,40,apv,The Little Mermaid,1989
-watch_event,age_40_50,20,net,30 for 30: Monaco,2020
+--watch_event,age_40_50,30,apv,Mulan,1998
+--watch_event,age_40_50,50,apv,Beauty and the Beast,1991
+--watch_event,age_40_50,40,apv,The Little Mermaid,1989
+--watch_event,age_40_50,20,net,30 for 30: Monaco,2020

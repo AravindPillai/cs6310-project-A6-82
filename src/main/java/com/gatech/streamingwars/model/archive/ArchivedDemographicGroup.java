5384854b9f1101f664aa1801f071cf12720782f1
aravindpillai <=>
2020-11-14 20:37:46 -0500

    This code has merege confirmed and committed

---
+++ src/main/java/com/gatech/streamingwars/model/archive/ArchivedDemographicGroup.java
@@ -0,0 +1,54 @@
+package com.gatech.streamingwars.model.archive;
+
+import com.gatech.streamingwars.model.AuditEntity;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "archived_demographic_group", schema = "archive")
+@Data
+public class ArchivedDemographicGroup extends AuditEntity {
+    @Id
+    Long id;
+    private String shortName;
+    private String longName;
+    private String description;
+
+    public ArchivedDemographicGroup()
+    {
+
+    }
+
+    public ArchivedDemographicGroup(Long id,String shortName, String longName,String description) {
+        this.id = id;
+        this.shortName = shortName;
+        this.longName = longName;
+        this.description = description;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}

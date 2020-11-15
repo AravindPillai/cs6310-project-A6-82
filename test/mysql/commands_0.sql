32e6823907d9a4c9d188eb810f19f8afd3cf159f
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-15 18:28:42 -0500

    Update commands_0.sql

--- test/mysql/commands_0.sql
+++ test/mysql/commands_0.sql
@@ -1,13 +1,27 @@
-create_demo,age_40_50,Viewers between 40 and 50,800
-create_demo,age_20_anime,Viewers under 20 who watch anime weekly,2000
-create_studio,disney,Walt Disney Animation Studios
-create_studio,espn,Entertainment Sports Network Studios
+-- Demo inserts
 
-create_event,movie,Mulan,1998,88,disney,1000
-create_event,ppv,30 for 30: Monaco,2020,106,espn,3300
-create_event,movie,The Little Mermaid,1989,83,disney,2000
-create_event,movie,Beauty and the Beast,1991,84,disney,1000
-create_event,ppv,MMA Championship,2020,121,espn,8800
+INSERT INTO DemographicGroup (shortName, longName) VALUES ('age_40_50','Viewers between 40 and 50',800);
+INSERT INTO DemographicGroup (shortName, longName) VALUES ('age_20_anime','Viewers under 20 who watch anime weekly',2000);
+
+-- Studio inserts
+
+INSERT INTO Studio (shortName, longName) VALUES ('disney','Walt Disney Animation Studios');
+INSERT INTO Studio (shortName, longName) VALUES ('espn','Entertainment Sports Network Studios');
+
+-- Event inserts
+-- Movie type
+
+INSERT INTO Event (eventType,name,year,duration,studioShortName,eventLicensingFee) 
+VALUES ('movie','Mulan',1998,88,'disney',1000);
+
+INSERT INTO Event (eventType,name,year,duration,studioShortName,eventLicensingFee) 
+VALUES ('ppv','30 for 30: Monaco',2020,106,'espn',3300);
+
+INSERT INTO Event (eventType,name,year,duration,studioShortName,eventLicensingFee) 
+VALUES ('movie','Beauty and the Beast',1991,84,'disney',1000);
+
+INSERT INTO Event (eventType,name,year,duration,studioShortName,eventLicensingFee) 
+VALUES ('ppv','MMA Championship',2020,121,'espn',8800);
 
 create_stream,apv,Amazon Prime Video,12
 create_stream,net,Netflix,14

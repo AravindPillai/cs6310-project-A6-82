0b3b21ece118db582b152da99cf428278f78d34a
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-15 18:33:18 -0500

    Update commands_2.sql

--- test/mysql/commands_2.sql
+++ test/mysql/commands_2.sql
@@ -1,32 +1,24 @@
-create_studio,warner,Warner Brothers
-display_studio,warner
-create_event,movie,Batman v Superman,2016,152,warner,1000
-display_events
-create_stream,hulu,Hulu Plus,11
-display_stream,hulu
-offer_movie,hulu,Batman v Superman,2016
-display_offers
-display_studio,warner
-display_stream,hulu
-create_demo,age_20_heroes,Viewers of Marvel/DC under 20,10000
-display_demo,age_20_heroes
-watch_event,age_20_heroes,60,hulu,Batman v Superman,2016
-display_demo,age_20_heroes
-display_stream,hulu
-display_time
-next_month
-display_studio,warner
-display_stream,hulu
-display_demo,age_20_heroes
-display_time
-display_events
-display_offers
-offer_movie,hulu,Batman v Superman,2016
-display_offers
-display_studio,warner
-display_stream,hulu
-display_demo,age_20_heroes
-watch_event,age_20_heroes,30,hulu,Batman v Superman,2016
-display_demo,age_20_heroes
-display_stream,hulu
-stop
+-- Studio inserts
+
+INSERT INTO Studio (shortName, longName) VALUES ('warner','Warner Brothers');
+
+-- Event inserts
+-- Movie type
+
+INSERT INTO Event (eventType,name,year,duration,studioShortName,eventLicensingFee) 
+VALUES ('movie','Batman v Superman',2016,152,'warner',1000);
+
+--create_stream,hulu,Hulu Plus,11
+
+--offer_movie,hulu,Batman v Superman,2016
+
+-- Demo inserts
+
+INSERT INTO DemographicGroup (shortName, longName) VALUES ('age_20_heroes','Viewers of Marvel/DC under 20',10000);
+
+--watch_event,age_20_heroes,60,hulu,Batman v Superman,2016
+
+--offer_movie,hulu,Batman v Superman,2016
+
+--watch_event,age_20_heroes,30,hulu,Batman v Superman,2016
+

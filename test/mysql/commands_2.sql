e9357adfb032855b3cd7f235300da5ee89aff659
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-16 08:34:24 -0500

    Added stream inserts need Stream table, fee column

--- test/mysql/commands_2.sql
+++ test/mysql/commands_2.sql
@@ -8,7 +8,11 @@ INSERT INTO Studio (shortName, longName) VALUES ('warner','Warner Brothers');
 INSERT INTO Event (eventType,name,year,duration,studioShortName,eventLicensingFee) 
 VALUES ('movie','Batman v Superman',2016,152,'warner',1000);
 
---create_stream,hulu,Hulu Plus,11
+-- Stream inserts
+
+--INSERT INTO StreamingServices (shortName, longName, streamFee) VALUES ('hulu','Hulu Plus',11);
+
+-- Offer Movie 
 
 --offer_movie,hulu,Batman v Superman,2016
 
@@ -16,9 +20,15 @@ VALUES ('movie','Batman v Superman',2016,152,'warner',1000);
 
 INSERT INTO DemographicGroup (shortName, longName) VALUES ('age_20_heroes','Viewers of Marvel/DC under 20',10000);
 
+-- Watch Event
+
 --watch_event,age_20_heroes,60,hulu,Batman v Superman,2016
 
+-- Offer Movie 
+
 --offer_movie,hulu,Batman v Superman,2016
+
+-- Watch Event
 
 --watch_event,age_20_heroes,30,hulu,Batman v Superman,2016
 

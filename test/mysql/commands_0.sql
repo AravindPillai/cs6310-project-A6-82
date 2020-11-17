6191a4005141b421f807a9a2580ed918105e6a64
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-17 11:59:40 -0500

    Updated account inserts

--- test/mysql/commands_0.sql
+++ test/mysql/commands_0.sql
@@ -47,41 +47,41 @@
 INSERT INTO demographicgroup (shortName, longName, numberofaccounts) VALUES ('age_40_50','Viewers between 40 and 50', 10);
 
 -- Create accounts
-INSERT INTO account (id, name) VALUES (1,'age_40_50');
-INSERT INTO account (id, name) VALUES (2,'age_40_50');
-INSERT INTO account (id, name) VALUES (3,'age_40_50');
-INSERT INTO account (id, name) VALUES (4,'age_40_50');
-INSERT INTO account (id, name) VALUES (5,'age_40_50');
-INSERT INTO account (id, name) VALUES (6,'age_40_50');
-INSERT INTO account (id, name) VALUES (7,'age_40_50');
-INSERT INTO account (id, name) VALUES (8,'age_40_50');
-INSERT INTO account (id, name) VALUES (9,'age_40_50');
-INSERT INTO account (id, name) VALUES (10,'age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (name) VALUES ('age_40_50');
 
 
 INSERT INTO demographicgroup (shortname, longname,numberOfAccounts) VALUES ('age_20_anime','Viewers under 20 who watch anime weekly',20);
 
 -- Create accounts
-INSERT INTO account (id, name) VALUES (11,'age_20_anime');
-INSERT INTO account (id, name) VALUES (12,'age_20_anime');
-INSERT INTO account (id, name) VALUES (13,'age_20_anime');
-INSERT INTO account (id, name) VALUES (14,'age_20_anime');
-INSERT INTO account (id, name) VALUES (15,'age_20_anime');
-INSERT INTO account (id, name) VALUES (16,'age_20_anime');
-INSERT INTO account (id, name) VALUES (17,'age_20_anime');
-INSERT INTO account (id, name) VALUES (18,'age_20_anime');
-INSERT INTO account (id, name) VALUES (19,'age_20_anime');
-INSERT INTO account (id, name) VALUES (20,'age_20_anime');
-INSERT INTO account (id, name) VALUES (21,'age_20_anime');
-INSERT INTO account (id, name) VALUES (22,'age_20_anime');
-INSERT INTO account (id, name) VALUES (23,'age_20_anime');
-INSERT INTO account (id, name) VALUES (24,'age_20_anime');
-INSERT INTO account (id, name) VALUES (25,'age_20_anime');
-INSERT INTO account (id, name) VALUES (26,'age_20_anime');
-INSERT INTO account (id, name) VALUES (27,'age_20_anime');
-INSERT INTO account (id, name) VALUES (28,'age_20_anime');
-INSERT INTO account (id, name) VALUES (29,'age_20_anime');
-INSERT INTO account (id, name) VALUES (30,'age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (name) VALUES ('age_20_anime');
 
 -- Studio inserts
 

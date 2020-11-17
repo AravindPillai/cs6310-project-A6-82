bd6e8c14ffe54abc9da31179b0b9458577f5a0c4
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-17 17:55:51 -0500

    new offer movie,ppv and transaction inserts

--- test/mysql/commands_0.sql
+++ test/mysql/commands_0.sql
@@ -29,22 +29,26 @@
 --     ,longname VARCHAR(100) NOT NULL
 --                               ,subscriptionprice INT NOT NULL);
 
+-- id/createdAt/currentMonthYear/updated_at/buyer/eventDuration/eventName/eventType/eventYear/percentage/ppvCost/transactionCost/transactionType/vendor
 -- create table transaction (
---                                   id INT AUTO_INCREMENT PRIMARY KEY
---     ,transactiontype VARCHAR(100) NULL
+--     id INT AUTO_INCREMENT PRIMARY KEY
+--     ,createdAt DATETIME NULL
+--     ,currentMonthYear VARCHAR(100) NULL
+--     ,updatedAt DATETIME NULL
+--     ,transactionType VARCHAR(100) NULL
 --     ,buyer VARCHAR(100) NULL
 --     ,vendor VARCHAR(100) NULL
---     ,eventtype VARCHAR(100) NULL
---     ,eventname VARCHAR(100) NULL
---     ,eventyear INT NULL
---     ,ppvcost INT NULL
+--     ,eventType VARCHAR(100) NULL
+--     ,eventName VARCHAR(100) NULL
+--     ,eventYear INT NULL
+--     ,ppvCost INT NULL
 --     ,percentage INT NULL
 --     ,eventduration INT NULL
---     ,transactioncost INT NULL);
+--     ,transactionCost INT NULL);
 
 -- Demo inserts
-
-INSERT INTO demographicgroup (shortName, longName, numberofaccounts) VALUES ('age_40_50','Viewers between 40 and 50', 10);
+INSERT INTO demographicgroup (shortName, longName, numberofaccounts) 
+VALUES ('age_40_50','Viewers between 40 and 50', 10);
 
 -- Create accounts
 INSERT INTO account (name) VALUES ('age_40_50');
@@ -58,8 +62,8 @@ INSERT INTO account (name) VALUES ('age_40_50');
 INSERT INTO account (name) VALUES ('age_40_50');
 INSERT INTO account (name) VALUES ('age_40_50');
 
-
-INSERT INTO demographicgroup (shortname, longname,numberOfAccounts) VALUES ('age_20_anime','Viewers under 20 who watch anime weekly',20);
+INSERT INTO demographicgroup (shortname, longname,numberOfAccounts) 
+VALUES ('age_20_anime','Viewers under 20 who watch anime weekly',20);
 
 -- Create accounts
 INSERT INTO account (name) VALUES ('age_20_anime');
@@ -84,13 +88,13 @@ INSERT INTO account (name) VALUES ('age_20_anime');
 INSERT INTO account (name) VALUES ('age_20_anime');
 
 -- Studio inserts
+INSERT INTO studio (shortname, longname) 
+VALUES ('disney','Walt Disney Animation Studios');
 
-INSERT INTO studio (shortname, longname) VALUES ('disney','Walt Disney Animation Studios');
-INSERT INTO studio (shortname, longname) VALUES ('espn','Entertainment Sports Network Studios');
+INSERT INTO studio (shortname, longname) 
+VALUES ('espn','Entertainment Sports Network Studios');
 
 -- Event inserts
--- Movie type
-
 INSERT INTO event (eventtype,name,year,duration,studioshortname,eventlicensingfee)
 VALUES ('movie','Mulan',1998,88,'disney',1000);
 
@@ -104,19 +108,31 @@ INSERT INTO event (eventtype,name,year,duration,studioshortname,eventlicensingfe
 VALUES ('ppv','MMA Championship',2020,121,'espn',8800);
 
 -- Stream inserts
+INSERT INTO streamingservice (shortname, longname, subscriptionprice) 
+VALUES ('apv','Amazon Prime Video',12);
 
-INSERT INTO streamingservice (shortname, longname, subscriptionprice) VALUES ('apv','Amazon Prime Video',12);
-INSERT INTO streamingservice (shortname, longname, subscriptionprice) VALUES ('net','Netflix',14);
+INSERT INTO streamingservice (shortname, longname, subscriptionprice) 
+VALUES ('net','Netflix',14);
 
--- offer_movie,apv,Mulan,1998
--- offer_movie,apv,The Little Mermaid,1989
--- offer_movie,apv,Beauty and the Beast,1991
--- offer_ppv,net,30 for 30: Monaco,2020,57
+-- Offer movie inserts
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',30,'movie','Mulan',1998,'watch',0,'apv',0,1000);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',50,'movie','Beauty and the Beast',1991,'watch',0,'apv',0,2000);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',40,'movie','The Little Mermaid',1989,'watch',0,'apv',0,1500);
 
+-- Offer ppv inserts
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',20,'ppv','30 for 30: Monaco',2020,'watch',0,'net',1000,0);
 
 -- Transaction inserts
-INSERT INTO transaction (buyer, percentage, eventtype ,eventname,eventyear) VALUES ('age_40_50',30,'apv','Mulan',1998);
-INSERT INTO transaction (buyer, percentage, eventtype,eventname,eventyear) VALUES ('age_40_50',50,'apv','Beauty and the Beast',1991);
-INSERT INTO transaction (buyer, percentage, eventtype,eventname,eventyear) VALUES ('age_40_50',40,'apv','The Little Mermaid',1989);
-INSERT INTO transaction (buyer, percentage, eventtype,eventname,eventyear) VALUES ('age_40_50',20,'net','30 for 30: Monaco',2020);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',30,'stream','Mulan',1998,'watch',0,'apv',0,0);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',50,'stream','Beauty and the Beast',1991,'watch',0,'apv',0,0);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',40,'stream','The Little Mermaid',1989,'watch',0,'apv',0,0);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',20,'stream','30 for 30: Monaco',2020,'watch',0,'net',0,0);
 

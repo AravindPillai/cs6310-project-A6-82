d7ed7d0cc00c883699cf16dc843e073164e30b8d
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-17 18:07:00 -0500

    all inserts updated ready for testing

--- test/mysql/commands_0.sql
+++ test/mysql/commands_0.sql
@@ -1,11 +1,13 @@
 -- create table demographicgroup (
 -- id INT AUTO_INCREMENT PRIMARY KEY
+--     ,createdAt DATETIME NULL
 -- ,shortName VARCHAR(100) NOT NULL
 -- ,longName VARCHAR(100) NOT NULL
 -- ,numberOfAccounts INT NOT NULL);
 
 -- create table event (
 -- id INT AUTO_INCREMENT PRIMARY KEY
+--     ,createdAt DATETIME NULL
 -- ,name VARCHAR(100) NOT NULL
 -- ,eventtype VARCHAR(100) NOT NULL
 --      ,studioshortname VARCHAR(100) NOT NULL
@@ -16,15 +18,18 @@
 
 --  create table account (
 --     id INT AUTO_INCREMENT PRIMARY KEY
+--     ,createdAt DATETIME NULL
 --     ,name VARCHAR(100) NOT NULL);
 
 -- create table studio (
 --                           id INT AUTO_INCREMENT PRIMARY KEY
+--     ,createdAt DATETIME NULL
 --      ,shortname VARCHAR(100) NOT NULL
 --      ,longname VARCHAR(100) NOT NULL);
 
 -- create table streamingservice (
 --                           id INT AUTO_INCREMENT PRIMARY KEY
+--     ,createdAt DATETIME NULL
 --    ,shortname VARCHAR(100) NOT NULL
 --     ,longname VARCHAR(100) NOT NULL
 --                               ,subscriptionprice INT NOT NULL);
@@ -47,84 +52,84 @@
 --     ,transactionCost INT NULL);
 
 -- Demo inserts
-INSERT INTO demographicgroup (shortName, longName, numberofaccounts) 
-VALUES ('age_40_50','Viewers between 40 and 50', 10);
+INSERT INTO demographicgroup (createdAt,shortName, longName, numberofaccounts)
+VALUES ('2020-11-17 04:00:00','age_40_50','Viewers between 40 and 50', 10);
 
 -- Create accounts
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
-INSERT INTO account (name) VALUES ('age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_40_50');
 
-INSERT INTO demographicgroup (shortname, longname,numberOfAccounts) 
-VALUES ('age_20_anime','Viewers under 20 who watch anime weekly',20);
+INSERT INTO demographicgroup (createdAt,shortname, longname,numberOfAccounts)
+VALUES ('2020-11-17 04:00:00','age_20_anime','Viewers under 20 who watch anime weekly',20);
 
 -- Create accounts
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
-INSERT INTO account (name) VALUES ('age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_anime');
 
 -- Studio inserts
-INSERT INTO studio (shortname, longname) 
-VALUES ('disney','Walt Disney Animation Studios');
+INSERT INTO studio (createdAt,shortname, longname)
+VALUES ('2020-11-17 04:00:00','disney','Walt Disney Animation Studios');
 
-INSERT INTO studio (shortname, longname) 
-VALUES ('espn','Entertainment Sports Network Studios');
+INSERT INTO studio (createdAt,shortname, longname)
+VALUES ('2020-11-17 04:00:00','espn','Entertainment Sports Network Studios');
 
 -- Event inserts
-INSERT INTO event (eventtype,name,year,duration,studioshortname,eventlicensingfee)
-VALUES ('movie','Mulan',1998,88,'disney',1000);
+INSERT INTO event (createdAt,eventtype,name,year,duration,studioshortname,eventlicensingfee)
+VALUES ('2020-11-17 04:00:00','movie','Mulan',1998,88,'disney',1000);
 
-INSERT INTO event (eventtype,name,year,duration,studioshortname,eventlicensingfee)
-VALUES ('ppv','30 for 30: Monaco',2020,106,'espn',3300);
+INSERT INTO event (createdAt,eventtype,name,year,duration,studioshortname,eventlicensingfee)
+VALUES ('2020-11-17 04:00:00','ppv','30 for 30: Monaco',2020,106,'espn',3300);
 
-INSERT INTO event (eventtype,name,year,duration,studioshortname,eventlicensingfee)
-VALUES ('movie','Beauty and the Beast',1991,84,'disney',1000);
+INSERT INTO event (createdAt,eventtype,name,year,duration,studioshortname,eventlicensingfee)
+VALUES ('2020-11-17 04:00:00','movie','Beauty and the Beast',1991,84,'disney',1000);
 
-INSERT INTO event (eventtype,name,year,duration,studioshortname,eventlicensingfee)
-VALUES ('ppv','MMA Championship',2020,121,'espn',8800);
+INSERT INTO event (createdAt,eventtype,name,year,duration,studioshortname,eventlicensingfee)
+VALUES ('2020-11-17 04:00:00','ppv','MMA Championship',2020,121,'espn',8800);
 
 -- Stream inserts
-INSERT INTO streamingservice (shortname, longname, subscriptionprice) 
-VALUES ('apv','Amazon Prime Video',12);
+INSERT INTO streamingservice (createdAt,shortname, longname, subscriptionprice)
+VALUES ('2020-11-17 04:00:00','apv','Amazon Prime Video',12);
 
-INSERT INTO streamingservice (shortname, longname, subscriptionprice) 
-VALUES ('net','Netflix',14);
+INSERT INTO streamingservice (createdAt,shortname, longname, subscriptionprice)
+VALUES ('2020-11-17 04:00:00','net','Netflix',14);
 
 -- Offer movie inserts
 INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
-VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',30,'movie','Mulan',1998,'watch',0,'apv',0,1000);
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',30,'movie','Mulan',1998,'offer',0,'apv',0,1000);
 INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
-VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',50,'movie','Beauty and the Beast',1991,'watch',0,'apv',0,2000);
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',50,'movie','Beauty and the Beast',1991,'offer',0,'apv',0,2000);
 INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
-VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',40,'movie','The Little Mermaid',1989,'watch',0,'apv',0,1500);
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',40,'movie','The Little Mermaid',1989,'offer',0,'apv',0,1500);
 
 -- Offer ppv inserts
 INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
-VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',20,'ppv','30 for 30: Monaco',2020,'watch',0,'net',1000,0);
+VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',20,'ppv','30 for 30: Monaco',2020,'offer',0,'net',1000,0);
 
 -- Transaction inserts
 INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
@@ -135,4 +140,6 @@ INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype
 VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',40,'stream','The Little Mermaid',1989,'watch',0,'apv',0,0);
 INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
 VALUES ('2020-11-17 04:00:00','11-2020','age_40_50',20,'stream','30 for 30: Monaco',2020,'watch',0,'net',0,0);
+
+
 

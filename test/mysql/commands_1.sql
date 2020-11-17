a262f77d9f9f9041926ddbd2d9c016a7aaaabe32
clfountain3 <73506571+clfountain3@users.noreply.github.com>
2020-11-17 18:16:29 -0500

    updated and ready for testing

--- test/mysql/commands_1.sql
+++ test/mysql/commands_1.sql
@@ -1,37 +1,44 @@
 -- Studio inserts
-INSERT INTO studio (shortname, longname) VALUES ('warner','Warner Brothers');
+INSERT INTO studio (createdAt,shortname, longname)
+VALUES ('2020-11-17 04:00:00','warner','Warner Brothers');
 
 -- Event inserts
--- Movie type
-INSERT INTO event (eventtype,name,year,duration,studioshortname,eventlicensingfee)
-VALUES ('movie','Batman v Superman',2016,152,'warner',1000);
+INSERT INTO event (createdAt,eventtype,name,year,duration,studioshortname,eventlicensingfee)
+VALUES ('2020-11-17 04:00:00','movie','Batman v Superman',2016,152,'warner',1000);
 
 -- Stream inserts
-INSERT INTO streamingservice (shortname, longname, subscriptionprice) VALUES ('hulu','Hulu Plus',11);
+INSERT INTO streamingservice (createdAt,shortname, longname, subscriptionprice)
+VALUES ('2020-11-17 04:00:00','hulu','Hulu Plus',11);
 
--- Offer Movie 
--- offer_movie,hulu,Batman v Superman,2016
+-- Offer movie inserts
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_20_heroes',30,'movie','Batman v Superman',2016,'offer',0,'hulu',0,1000);
 
 -- Demo inserts
-INSERT INTO DemographicGroup (shortName, longName) VALUES ('age_20_heroes','Viewers of Marvel/DC under 20',10);
+INSERT INTO demographicgroup (createdAt,shortname, longname,numberOfAccounts)
+VALUES ('2020-11-17 04:00:00','age_20_heroes','Viewers of Marvel/DC under 20',10);
 
 -- Create accounts
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
-INSERT INTO account (name) VALUES ('age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
+INSERT INTO account (createdAt,name) VALUES ('2020-11-17 04:00:00','age_20_heroes');
 
 -- Transaction inserts
-INSERT INTO transaction (buyer, percentage, eventtype ,eventname,eventyear) VALUES ('age_20_heroes',60,'hulu','Batman v Superman',2016);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_20_heroes',60,'stream','Batman v Superman',2016,'watch',0,'hulu',0,0);
 
--- Offer Movie 
--- offer_movie,hulu,Batman v Superman,2016
+-- Offer movie inserts
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_20_heroes',20,'movie','Batman v Superman',2016,'offer',0,'hulu',0,1000);
 
 -- Transaction inserts
-INSERT INTO transaction (buyer, percentage, eventtype ,eventname,eventyear) VALUES ('age_20_heroes',30,'hulu','Batman v Superman',2016);
+INSERT INTO transaction (createdAt,currentMonthYear,buyer, percentage, eventtype ,eventname,eventyear,transactiontype,ppvcost,vendor,eventduration,transactionCost)
+VALUES ('2020-11-17 04:00:00','11-2020','age_20_heroes',30,'stream','Batman v Superman',2016,'watch',0,'hulu',0,0);
+
